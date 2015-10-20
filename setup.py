94312ec15a6240e4179ff1c6bc56c5040d434f26
Forrest Pieper <forrestp@nvbots.com>
2015-10-20 06:52:18 -0400

    add setup script, .gitignore for build dir

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='iw_parse',
+      version='0.1',
+      description='parser for iwlist wifi scan',
+      url='https://github.com/NVBots/iw_parse',
+      packages=['iw_parse'],
+     )
